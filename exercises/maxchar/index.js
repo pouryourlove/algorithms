ee47b9357c3d9cedbca737262a0803495af59cf5
Juyeon Lee <lqu134@naver.com>
2022-12-19 17:58:47 +0900

    MaxChars

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const object = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    if (!object[char]) {
+      object[char] = 1;
+    } else {
+      object[char]++;
+    }
+  }
+  for (let char in object) {
+    if (object[char] > max) {
+      max = object[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
