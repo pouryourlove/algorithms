be4af7e2e37094b5da4b0f90f718570486c24d23
pouryourlove <lqu134@naver.com>
2023-01-08 23:44:29 +0900

    until RemoveAt Solution

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -54,6 +54,59 @@ class LinkedList {
     }
     this.head = this.head.next;
   }
+  removeLast() {
+    if (!this.head) {
+      return;
+    }
+    if (!this.head.next) {
+      this.head = null;
+      return;
+    }
+    let previous = this.head;
+    let node = this.head.next;
+    while (node.next) {
+      previous = node;
+      node = node.next;
+    }
+    previous.next = null;
+  }
+  insertLast(data) {
+    const last = this.getLast();
+    if (last) {
+      //There are some exisiting nodes in our chain
+      last.next = new Node(data);
+    } else {
+      //The chain is empty!
+      this.head = new Node(data);
+    }
+  }
+  getAt(index) {
+    let counter = 0;
+    let node = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+      counter++;
+      node = node.next;
+    }
+    return null;
+  }
+
+  removeAt(index) {
+    if (!this.head) {
+      return;
+    }
+    if (index === 0) {
+      this.head = this.head.next;
+      return;
+    }
+    const previous = this.getAt(index - 1);
+    if (!previous || !previous.next) {
+      return;
+    }
+    previous.next = previous.next.next;
+  }
 }
 
 const list = (module.exports = { Node, LinkedList });
